644c9db1d47a4c593d58ab5d4df75f2262369957
Alvaro Barrera <alvaro_bv@hotmail.com>
2021-05-02 11:28:16 -0500

    Publish component jetstream view

--- resources/views/components/course-card.blade.php
+++ resources/views/components/course-card.blade.php
@@ -5,7 +5,6 @@
         <h2 class="text-lg text-gray-600 truncate uppercase font-semibold">{{ $course->name }}</h2>
         <h3 class="text-md text-gray-500">{{ $course->excerpt }}</h3>
 
-        <img src="{{ $course->user->avatar }}" alt="{{ $course->user->name }}"
-             class="rounded-full mx-auto mt-4 h-16 w-16">
+        <img src="{{ $course->user->avatar }}" alt="{{ $course->user->name }}" class="rounded-full mx-auto mt-4 h-16 w-16">
     </a>
 </div>
